147779584c921df2b0f9ae0d2c2c3011cdd19212
geek-ragazza <tecnologia.ragazza@gmail.com>
2017-07-31 19:19:54 +0800

    remove south from install requires

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
     install_requires=[
         "django >= 1.3, < 1.9",
         "boto3 >= 1.1",
-        "South >= 0.8",
     ],
     classifiers=[
         "Intended Audience :: Developers",
